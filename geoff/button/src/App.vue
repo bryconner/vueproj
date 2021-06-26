1d1b1534c0f55564a11d9e4988f1d883d19f50cf
Bryce Conner <bryce.webmaster@gmail.com>
2021-06-26 14:48:54 -0400

    test

---
+++ geoff/button/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <input v-model="outbox" name="outbox" />
+    <button @click="sendtoinbox">Send to Inbox</button>
+    <h3>[{{ inbox }}]</h3>
+</template>
+
+<script>
+export default {
+   name: "App",
+   data() {
+       return { outbox: "", inbox: "empty", }
+   }, 
+    methods: {
+        sendtoinbox() {
+            this.inbox = this.outbox;
+        },
+    },
+}
+</script>
