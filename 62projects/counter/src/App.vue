de794bd0862e23dca29b9e06e270dadae194ed7e
Bryce Conner <bryce.webmaster@gmail.com>
2021-08-02 17:30:56 -0400

    initial add

---
+++ 62projects/counter/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button v-on:click="count++">increment</button>
+    <button v-on:click="count--">decrement</button>
+    <p>{{ count }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return { count: 0 };
+  },
+};
+</script>
---
+++ 62projects/image-slider/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+       <img :src="image" />
+   </div>
+</template>
+
+<script>
+const images = [
+    "https://i.picsum.photos/id/1/200/300.jpg?hmac=jH5bDkLr6Tgy3oAg5khKCHeunZMHq0ehBZr6vGifPLY",
+    "https://i.picsum.photos/id/2/200/300.jpg?hmac=HiDjvfge5yCzj935PIMj1qOf4KtvrfqWX3j4z1huDaU",
+    "https://i.picsum.photos/id/3/200/300.jpg?hmac=o1-38H2y96Nm7qbRf8Aua54lF97OFQSHR41ATNErqFc",
+];
+export default {
+    name:"App",
+    data() {
+        return { index: 0, image: images[0]};
+    },
+methods: {
+    next() {
+        this.index = (this.index + 1) % images.length;
+        this.image = images[this.index];
+    },
+    autoChangeSlide() {
+        setInterval(() => {
+            this.next();
+        }, 1000);
+    },
+  },
+  beforeMount() {
+     this.autoChangeSlide(); 
+  },   
+};
+</script>
